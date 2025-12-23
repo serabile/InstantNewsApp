6852a08ebb91ab1787a869b387758a913c1e7d30
Nabil Serbout <serabile@gmail.com>
2025-12-23 12:11:11 +0100

    Add logo to Top News screen title

--- features/topnews/src/main/java/com/serabile/topnews/screen/TopNewsScreen.kt
+++ features/topnews/src/main/java/com/serabile/topnews/screen/TopNewsScreen.kt
@@ -1,9 +1,14 @@
 package com.serabile.topnews.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -11,14 +16,17 @@ import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +57,23 @@ fun TopNewsScreen(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(com.serabile.topnews.R.string.top_news_title)) },
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = com.serabile.designsystem.R.drawable.ic_launcher),
+                            contentDescription = null,
+                            modifier = Modifier.size(72.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = stringResource(com.serabile.topnews.R.string.top_news_title),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+                },
+
                 scrollBehavior = scrollBehavior,
                 actions = {
                     IconButton(onClick = { viewModel.processIntent(TopNewsIntent.LoadOrRefresh) }) {
