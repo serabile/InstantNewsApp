b129cc96111a81a3b810c5cc182ce446cd5efeac
Nabil Serbout <serabile@gmail.com>
2025-12-23 12:19:41 +0100

    Add spotless check for code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,24 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.spotless)
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("**/build/**/*.kt")
+        ktlint("1.5.0")
+            .editorConfigOverride(
+                mapOf(
+                    "ktlint_standard_no-wildcard-imports" to "disabled",
+                    "ktlint_standard_filename" to "disabled"
+                )
+            )
+    }
+    
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint("1.5.0")
+    }
 }
